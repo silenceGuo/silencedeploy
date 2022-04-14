74bb2af8d54a22708131140ebb222946535206cb
健康商城颐而康股份公司 <jingcui_2019@163.com>
2022-04-14 09:09:47 +0800

    init

---
+++ main.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python 
+# -*- coding: utf-8 -*- 
+# @Time : 2022-4-14 上午 9:00 
+# @Author : damon.guo 
+# @File : main.py 
+# @Software: PyCharm
+def main():
+    print("hello,world")
+
+
+if __name__ == "__main__":
+    main()
